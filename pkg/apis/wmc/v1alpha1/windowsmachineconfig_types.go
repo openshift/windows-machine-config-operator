053f52b098d9bbaebee9718c72dd82311e764bc5
ravisantoshgudimetla <ravisantoshgudimetla@gmail.com>
2020-02-18 15:55:47 -0500

    [wmco] Barebone WMCO initialization
    Followed the steps at
    https://github.com/operator-framework/operator-sdk/blob/master/doc/user-guide.md
    to initialize the repo, as of now, following items are added:
    
        - A new apiVersion called wmc.openshift.io/v1alpha1
        - A new kind called WindowsMachineConfig
        - A deployable CRD, CR, rbac and sa
        - Controller which watches for WindowsMachineConfig CRs
    
    The goal is to have the operator watch for windowsMachineConfig CR and
    windowsMachineConfig Configmap in future. As of now, we're just going to watch
    for the windowsConfig CR(which is singleton)
    
    Following are the commands, that I ran:
    
        - operator-sdk new windows-machine-config-operator \
    	--repo=github.com/example-inc/memcached-operator
        - operator-sdk add api --api-version=wmc.openshift.io/v1alpha1 \
          --kind=WindowsMachineConfig
        - operator-sdk generate k8s
        - operator-sdk generate crds
        - operator-sdk add controller --api-version=wmc.openshift.io/v1alpha1 \
          --kind=WindowsMachineConfig

---
+++ pkg/apis/wmc/v1alpha1/windowsmachineconfig_types.go
@@ -0,0 +1,76 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// WindowsMachineConfigSpec defines the desired state of WindowsMachineConfig
+type WindowsMachineConfigSpec struct {
+	// Replicas represent how many Windows nodes to be added to the
+	// OpenShift cluster
+	Replicas int `json:"replicas,omitempty"`
+	// InstanceType represents the flavor of instance to be used while
+	// creating the virtual machines. Please note that this is common
+	// across all the Windows nodes in the cluster
+	InstanceType string `json:"instanceType,omitempty"`
+	// AWS holds AWS specific cloud provider information.
+	AWS *AWS `json:"aws"`
+	// Azure holds Azure specific cloud provider information
+	Azure *Azure `json:"azure"`
+	// CloudProviderCredentials is the name of the secret which contains the
+	// credentials of the cloud provider
+	CloudProviderCredentials string `json:"cloudProviderCredentials,omitempty"`
+}
+
+// AWS holds the information related to AWS cloud provider
+type AWS struct {
+	// SSHKeyPair is the sshKeyPair associated with cloudprovider. AWS
+	// asks a keypair to be present for encrypting the Windows VM password
+	SSHKeyPair string `json:"sshKeyPair"`
+	// SSHPrivateKey is the name of the secret which contains ssh key
+	// to decrypt the password
+	SSHPrivateKey string `json:"sshPrivateKey"`
+	// CredentialAccountID is account id associated with AWS provider
+	CredentialAccountID string `json:"credentialAccountId"`
+}
+
+// AzureProvider holds the information related to Azure cloud provider.
+// TODO: Populate when we are working on azure.
+type Azure struct {
+}
+
+// WindowsMachineConfigStatus defines the observed state of WindowsMachineConfig
+type WindowsMachineConfigStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// WindowsMachineConfig is the Schema for the windowsmachineconfigs API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=windowsmachineconfigs,scope=Namespaced
+type WindowsMachineConfig struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   WindowsMachineConfigSpec   `json:"spec,omitempty"`
+	Status WindowsMachineConfigStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// WindowsMachineConfigList contains a list of WindowsMachineConfig
+type WindowsMachineConfigList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []WindowsMachineConfig `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&WindowsMachineConfig{}, &WindowsMachineConfigList{})
+}
