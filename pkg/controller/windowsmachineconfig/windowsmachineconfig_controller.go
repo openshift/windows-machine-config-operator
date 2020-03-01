c5bbf7ac98e20ad7521a5247e8542dcf172e1ee1
ravisantoshgudimetla <ravisantoshgudimetla@gmail.com>
2020-02-29 19:59:54 -0500

    WMCO configuring nodes

--- pkg/controller/windowsmachineconfig/windowsmachineconfig_controller.go
+++ pkg/controller/windowsmachineconfig/windowsmachineconfig_controller.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/openshift/windows-machine-config-bootstrapper/tools/windows-node-installer/pkg/cloudprovider"
 	"github.com/openshift/windows-machine-config-bootstrapper/tools/windows-node-installer/pkg/types"
@@ -18,6 +19,31 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+const (
+	// remoteDir is the remote temporary directory that the e2e test uses
+	remoteDir = "C:\\Temp\\"
+	// winTemp is the default Windows temporary directory
+	winTemp = "C:\\Windows\\Temp\\"
+	// winCNIDir is the directory where the CNI files are placed
+	winCNIDir = winTemp + "\\cni\\"
+	// winCNIConfigPath is the CNI configuration file path on the Windows VM
+	winCNIConfigPath = "C:\\Windows\\Temp\\cni\\config\\"
+	// logDir is the remote kubernetes log director
+	kLog = "C:\\k\\log\\"
+	// cniConfigTemplate is the location of the cni.conf template file
+	cniConfigTemplate = "templates/cni.template"
+	// wgetIgnoreCertCmd is the remote location of the wget-ignore-cert.ps1 script
+	wgetIgnoreCertCmd = remoteDir + "wget-ignore-cert.ps1"
+	// e2eExecutable is the remote location of the WMCB e2e test binary
+	e2eExecutable = remoteDir + "wmcb_e2e_test.exe"
+	// unitExecutable is the remote location of the WMCB unit test binary
+	unitExecutable = remoteDir + "wmcb_unit_test.exe"
+	// hybridOverlayName is the name of the hybrid overlay executable
+	hybridOverlayName = "hybrid-overlay.exe"
+	// hybridOverExecutable is the remote location of the hybrid overlay binary
+	hybridOverlayExecutable = remoteDir + hybridOverlayName
 )
 
 var log = logf.Log.WithName("controller_windowsmachineconfig")
@@ -75,7 +101,22 @@ type ReconcileWindowsMachineConfig struct {
 	client        client.Client
 	scheme        *runtime.Scheme
 	cloudProvider cloudprovider.Cloud
-	windowsVM     []types.WindowsVM
+	windowsVM     map[types.WindowsVM]bool
+}
+
+// windowsVM is a wrapper for the WindowsVM interface
+type windowsVM struct {
+	types.WindowsVM
+}
+
+// pkg encapsulates information about a package
+type pkgInfo struct {
+	// url is the URL of the package
+	url string
+	// sha is the SHA hash of the package
+	sha string
+	// shaType is the type of SHA used, example: 256 or 512
+	shaType string
 }
 
 // Reconcile reads that state of the cluster for a WindowsMachineConfig object and makes changes based on the state read
@@ -116,20 +157,28 @@ func (r *ReconcileWindowsMachineConfig) Reconcile(request reconcile.Request) (re
 			"~/.aws/credentials",
 			"default",
 			"", "", instance.Spec.InstanceType,
-			"", "")
+			"openshift-dev", "/home/ravig/.ssh/openshift-dev.pem")
 		//}
 		if err != nil {
 			return reconcile.Result{}, fmt.Errorf("error instantianting cloud provider: %v", err)
 		}
 	}
-
+	if r.windowsVM == nil {
+		// populate the windowsVM map here from configmap as source of truth
+		r.windowsVM = make(map[types.WindowsVM]bool)
+	}
 	// Get the current count of required number of Windows VMs
 	currentCountOfWindowsVMs := 1 // As of now hardcoded to 1. We need to get the number of Windows VM node objects
 	if instance.Spec.Replicas != currentCountOfWindowsVMs {
+		if instance.Spec.Replicas == 0 {
+			instance.Spec.Replicas = 2
+		}
 		if err := r.reconcileWindowsNodes(instance.Spec.Replicas, currentCountOfWindowsVMs); err != nil {
 			return reconcile.Result{}, err
 		}
 	}
+
+	// Get the node objects and do a node count.
 
 	return reconcile.Result{}, nil
 }
@@ -158,33 +207,167 @@ func (r *ReconcileWindowsMachineConfig) reconcileCredentials() error {
 	return nil
 }
 
+func chooseRandomNode(windowsVMs map[types.WindowsVM]bool) types.WindowsVM {
+	for windowsVM := range windowsVMs {
+		return windowsVM
+	}
+	return nil
+}
+
 // deleteWindowsVMs returns the instance IDs of the successfully created Windows VMs
 func (r *ReconcileWindowsMachineConfig) deleteWindowsVMs(count int) {
 	// From the list of Windows VMs choose randomly count number of VMs. As of now sequential
 	for i := 0; i < count; i++ {
-		if r.windowsVM[i].GetCredentials() == nil {
+		vmTobeDeleted := chooseRandomNode(r.windowsVM)
+		if vmTobeDeleted.GetCredentials() == nil {
 			continue
 		}
-		if len(r.windowsVM[i].GetCredentials().GetInstanceId()) == 0 {
+		instancedID := vmTobeDeleted.GetCredentials().GetInstanceId()
+		if len(instancedID) == 0 {
 			continue
 		}
-		instance := r.windowsVM[i].GetCredentials().GetInstanceId()
-		// Delete the Windows VM here. Make a call to the Windows
-		log.Info("deleting the Windows VM %v", instance)
-		if err := r.cloudProvider.DestroyWindowsVM(instance); err != nil {
-			log.Error(err, "error while deleting windows VM %s", instance)
+		// Delete the Windows VM from cloud provider
+		log.Info("deleting the Windows VM", instancedID)
+		deleted := true
+		if err := r.cloudProvider.DestroyWindowsVM(instancedID); err != nil {
+			log.Error(err, "error while deleting windows VM %s", instancedID)
+			deleted = false
+		}
+		if deleted {
+			delete(r.windowsVM, vmTobeDeleted)
 		}
 	}
-
 }
+
+var (
+	// kubeNode contains the information about  the kubernetes node package for Windows
+	kubeNode = pkgInfo{
+		url:     "https://dl.k8s.io/v1.16.2/kubernetes-node-windows-amd64.tar.gz",
+		sha:     "a88e7a1c6f72ea6073dbb4ddfe2e7c8bd37c9a56d94a33823f531e303a9915e7a844ac5880097724e44dfa7f4a9659d14b79cc46e2067f6b13e6df3f3f1b0f64",
+		shaType: "sha512",
+	}
+)
 
 func (r *ReconcileWindowsMachineConfig) createWindowsVMs(count int) {
 	for i := 0; i < count; i++ {
-		// Create Windows VM
-		windowsVM, err := r.cloudProvider.CreateWindowsVM()
+		// Create Windows VM in the cloud provider
+		createdVM, err := r.cloudProvider.CreateWindowsVM()
 		if err != nil {
 			log.Error(err, "error while creating windows VM", err)
+			// return here
 		}
-		r.windowsVM = append(r.windowsVM, windowsVM)
+		log.V(0).Info("created the Windows VM", createdVM.GetCredentials().GetInstanceId())
+		log.V(0).Info("created the Windows VM ", createdVM.GetCredentials().GetPassword())
+		// Copy paste wmcb e2e code for prepping the node :)
+		vm := &windowsVM{createdVM}
+		vm.configureWindowsVM()
+
+		// update the windowsVM interface
+		if _, ok := r.windowsVM[createdVM]; !ok {
+			r.windowsVM[createdVM] = true
+		}
 	}
+}
+
+func (vm *windowsVM) configureWindowsVM() {
+	// Create the temp directory
+	_, _, err := vm.Run(mkdirCmd(remoteDir), false)
+	if err != nil {
+		log.Error(err, "unable to create remote directory", remoteDir)
+	}
+	file := "/home/ravig/go/src/github.com/openshift/windows-machine-config-operator/pkg/controller/windowsmachineconfig/powershell/wget-ignore-cert.ps1"
+	if err := vm.CopyFile(file, remoteDir); err != nil {
+		log.Error(err, "error while copying powershell script")
+	}
+	vm.runBootstrapper()
+}
+
+func (vm *windowsVM) runBootstrapper() {
+	if err := vm.CopyFile("/home/ravig/go/src/github.com/openshift/windows-machine-config-bootstrapper/wmcb.exe", remoteDir); err != nil {
+		log.Error(err, "error while copying wmcb binary")
+	}
+	err := vm.initializeTestBootstrapperFiles()
+	if err != nil {
+		log.Error(err, "error logging message")
+	}
+	_, _, err = vm.Run(remoteDir+"\\wmcb.exe initialize-kubelet --ignition-file "+winTemp+"worker.ign --kubelet-path "+winTemp+"kubelet.exe", true)
+	//log.Info("Stdout", stdout)
+	//log.Info("Stderr", stderr)
+	if err != nil {
+		log.Error(err, "error running bootstrapper")
+	}
+}
+
+// initializeTestBootstrapperFiles initializes the files required for initialize-kubelet
+func (vm *windowsVM) initializeTestBootstrapperFiles() error {
+	// Download and extract the kube package on the VM
+	err := vm.remoteDownloadExtract(kubeNode, remoteDir+"kube.tar.gz", remoteDir)
+	if err != nil {
+		return fmt.Errorf("unable to download kube package: %v", err)
+	}
+
+	// Copy kubelet.exe to C:\Windows\Temp\
+	_, _, err = vm.Run("cp "+remoteDir+"kubernetes\\node\\bin\\kubelet.exe "+winTemp, true)
+	if err != nil {
+		return fmt.Errorf("unable to copy kubelet.exe to %s", winTemp)
+	}
+	// TODO: As of now, getting cluster address from the environment var, remove it.
+	// Download the worker ignition to C:\Windows\Tenp\ using the script that ignores the server cert
+	ClusterAddress := os.Getenv("CLUSTER_ADDR")
+	fmt.Println("ravig", wgetIgnoreCertCmd+" -server https://api-int."+ClusterAddress+":22623/config/worker"+" -output "+winTemp+"worker.ign")
+	stdout, stderr, err := vm.Run(wgetIgnoreCertCmd+" -server https://api-int."+ClusterAddress+":22623/config/worker"+" -output "+winTemp+"worker.ign", true)
+	fmt.Printf("stdout %v", stdout)
+	fmt.Printf("Stderr %v", stderr)
+	if err != nil {
+		fmt.Printf("ravig %v", err)
+		return fmt.Errorf("unable to download worker.ign: %v", err)
+	}
+
+	return nil
+}
+
+// remoteDownload downloads the tar file in url to the remoteDownloadFile location and checks if the SHA matches
+func (vm *windowsVM) remoteDownload(pkg pkgInfo, remoteDownloadFile string) error {
+	_, stderr, err := vm.Run("if (!(Test-Path "+remoteDownloadFile+")) { wget "+pkg.url+" -o "+remoteDownloadFile+" }",
+		true)
+	if err != nil {
+		return fmt.Errorf("unable to download %s: %v\n%s", pkg.url, err, stderr)
+	}
+
+	if pkg.sha == "" {
+		return nil
+	}
+
+	// Perform a checksum check
+	stdout, _, err := vm.Run("certutil -hashfile "+remoteDownloadFile+" "+pkg.shaType, true)
+	if err != nil {
+		return fmt.Errorf("unable to check SHA of %s: %v", remoteDownloadFile, err)
+	}
+	if !strings.Contains(stdout, pkg.sha) {
+		return fmt.Errorf("package %s SHA does not match: %v\n%s", remoteDownloadFile, err, stdout)
+	}
+
+	return nil
+}
+
+// remoteDownloadExtract downloads the tar file in url to the remoteDownloadFile location, checks if the SHA matches and
+//  extracts the files to the remoteExtractDir directory
+func (vm *windowsVM) remoteDownloadExtract(pkg pkgInfo, remoteDownloadFile, remoteExtractDir string) error {
+	// Download the file from the URL
+	err := vm.remoteDownload(pkg, remoteDownloadFile)
+	if err != nil {
+		return fmt.Errorf("unable to download %s: %v", pkg.url, err)
+	}
+
+	// Extract files from the archive
+	_, stderr, err := vm.Run("tar -xf "+remoteDownloadFile+" -C "+remoteExtractDir, true)
+	if err != nil {
+		return fmt.Errorf("unable to extract %s: %v\n%s", remoteDownloadFile, err, stderr)
+	}
+	return nil
+}
+
+// mkdirCmd returns the Windows command to create a directory if it does not exists
+func mkdirCmd(dirName string) string {
+	return "if not exist " + dirName + " mkdir " + dirName
 }
