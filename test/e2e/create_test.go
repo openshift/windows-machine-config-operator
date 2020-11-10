c76caf56496e7dcfb5720c0dcad9a1af7f091dc2
pratikmahajan <pratik@mahajan.xyz>
2020-11-10 12:50:01 -0500

    fix k8s version  being misreported
    This PR fixes the bug where kubelet and kube-proxy were not
    reporting correct version.
    This was because of absence of git metadata which is required for
    setting this version.
    
    The bug fix includes copying the git metadata.
    Also, added test to check the kubelet version in-case it breaks again.
    We are just checking if the prefix has the major and minor version like `v1.19`
    Consolidated the node metadata test and included kubelet version there
    
    Also, simplified the way we build the promu binary, by using `go install .`

--- test/e2e/create_test.go
+++ test/e2e/create_test.go
@@ -38,11 +38,7 @@ func creationTestSuite(t *testing.T) {
 		return
 	}
 	t.Run("Network validation", testNetwork)
-	// The label is not actually added by WMCO however we would like to validate if the Machine Api is properly
-	// adding the worker label, if it was specified in the MachineSet. The MachineSet created in the test suite has
-	// the worker label
-	t.Run("Label validation", func(t *testing.T) { testWorkerLabel(t) })
-	t.Run("Version annotation", func(t *testing.T) { testVersionAnnotation(t) })
+	t.Run("Node Metadata", func(t *testing.T) { testNodeMetadata(t) })
 	t.Run("NodeTaint validation", func(t *testing.T) { testNodeTaint(t) })
 	t.Run("UserData validation", func(t *testing.T) { testUserData(t) })
 	t.Run("UserData idempotent check", func(t *testing.T) { testUserDataTamper(t) })
