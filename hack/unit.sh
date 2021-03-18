f40a5d28a3c1c0d9093f519cedf6be496c50628b
Sebastian Soto <ssoto@redhat.com>
2021-03-18 14:00:01 -0400

    Move main.go into base directory
    This is being done to move the repo to a kubebuilder style project
    layout in preparation for the OSDK version bump.

--- hack/unit.sh
+++ hack/unit.sh
@@ -7,5 +7,7 @@ cd "${WMCO_ROOT}"
 GOFLAGS=${1:-}
 
 # Run tests from all packages excluding e2e package, as it consists of e2e tests.
-go test -v ./cmd/... ./pkg/... ./controllers/... ${GOFLAGS} -count=1
+go test -v ./pkg/... ${GOFLAGS} -count=1
+go test -v ./controllers/... ${GOFLAGS} -count=1
+go test -v ./main.go ./main_test.go ${GOFLAGS} -count=1
 exit 0
