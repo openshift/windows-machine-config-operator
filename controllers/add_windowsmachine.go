19a831582dd2c59d3b0821420f4dfbb7bd19840c
Sebastian Soto <ssoto@redhat.com>
2021-03-18 14:25:44 -0400

    Move controllers into a single package
    This is being done to move the repo to a kubebuilder style project
    layout in preparation for the OSDK version bump. In order for this move
    to occur, functions and variables that shared the same name had to be
    renamed.

--- controllers/add_windowsmachine.go
+++ controllers/add_windowsmachine.go
@@ -1,10 +1,6 @@
-package controller
-
-import (
-	"github.com/openshift/windows-machine-config-operator/controllers/windowsmachine"
-)
+package controllers
 
 func init() {
 	// AddToManagerFuncs is a list of functions to create controllers and add them to a manager.
-	AddToManagerFuncs = append(AddToManagerFuncs, windowsmachine.Add)
+	AddToManagerFuncs = append(AddToManagerFuncs, AddWindowsMachineController)
 }
