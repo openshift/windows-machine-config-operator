dc8b738c3f018468454cce65fd15e35bb67207d7
Aravindh Puthiyaparambil <aravindh@redhat.com>
2021-04-28 19:09:17 -0400

    Fix node being shown as Ready after failed config
    - Cordon the node at beginning of a re-configuration or after the
    kubelet has been configured
    - Uncordon the node after successful configuration
    - In case of a configuration error, stop the kubelet before
      requeuing so that the node is marked as NotReady

--- pkg/windows/windows.go
+++ pkg/windows/windows.go
@@ -2,10 +2,11 @@ package windows
 
 import (
 	"fmt"
-	"github.com/go-logr/logr"
 	"path/filepath"
 	"strings"
 	"time"
+
+	"github.com/go-logr/logr"
 
 	oconfig "github.com/openshift/api/config/v1"
 	"github.com/pkg/errors"
@@ -135,6 +136,8 @@ type Windows interface {
 	ConfigureWindowsExporter() error
 	// ConfigureKubeProxy ensures that the kube-proxy service is running
 	ConfigureKubeProxy(string, string) error
+	// EnsureRequiredServicesStopped ensures that all services that are needed to configure a VM are stopped
+	EnsureRequiredServicesStopped() error
 }
 
 // windows implements the Windows interface
@@ -264,8 +267,7 @@ func (vm *windows) Reinitialize() error {
 	return nil
 }
 
-// ensureRequiredServicesStopped ensures that all services that are needed to configure a VM are stopped
-func (vm *windows) ensureRequiredServicesStopped() error {
+func (vm *windows) EnsureRequiredServicesStopped() error {
 	// This slice order matters due to service dependencies
 	requiredSVCs := []string{windowsExporterServiceName, kubeProxyServiceName, hybridOverlayServiceName,
 		kubeletServiceName}
@@ -280,7 +282,7 @@ func (vm *windows) ensureRequiredServicesStopped() error {
 
 func (vm *windows) Configure() error {
 	vm.log.Info("configuring")
-	if err := vm.ensureRequiredServicesStopped(); err != nil {
+	if err := vm.EnsureRequiredServicesStopped(); err != nil {
 		return errors.Wrap(err, "unable to stop required services")
 	}
 	// Set the hostName of the Windows VM in case of vSphere
