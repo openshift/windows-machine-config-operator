7c2ec5e774f9c00a7b865ce03ba17e3e0950b806
Sebastian Soto <ssoto@redhat.com>
2021-05-05 14:17:23 -0400

    [test] Standardize Machine names across platforms
    Makes the e2e aws Machineset naming schemes the same as the Azure and
    vSphere platforms for the purpose of consistency and being able to
    search through logs easier when switching between jobs on different
    platforms.
    
    Additionally makes the aws provider use the same label constant used by
    the other platforms.

--- test/e2e/clusterinfo/constants.go
+++ test/e2e/clusterinfo/constants.go
@@ -8,3 +8,15 @@ const (
 	MachineAPINamespace = "openshift-machine-api"
 	UserDataSecretName  = "windows-user-data"
 )
+
+// WindowsMachineSetName returns the name of the Windows MachineSet created in the e2e tests depending on if the
+// Windows label is set or not
+// TODO: Move this function to the providers package as part of https://issues.redhat.com/browse/WINC-608
+func WindowsMachineSetName(isWindowsLabelSet bool) string {
+	if isWindowsLabelSet {
+		// Designate MachineSets that set a Windows label on the Machine with
+		// "e2e-wm", to signify they should be configured by the Windows Machine controller
+		return "e2e-wm"
+	}
+	return "e2e"
+}
