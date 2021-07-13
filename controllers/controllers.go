34718d7a3c705783df4ba3cffb0ee79c40fd2035
Mohammad Saif Shaikh <mohashai@redhat.com>
2021-07-13 17:32:31 -0400

    delete old asymmetric encrypt/decrypt attempt

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,12 +1,6 @@
 package controllers
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/sha256"
-	"crypto/x509"
-	"encoding/base64"
-	"encoding/pem"
 	"net"
 
 	"github.com/go-logr/logr"
@@ -61,59 +55,6 @@ func (r *instanceReconciler) configureInstance(instance *instances.InstanceInfo,
 		return errors.Wrap(err, "failed to configure Windows instance")
 	}
 	return nil
-}
-
-// publicKeyRSA retrieves the public key used to access Windows instances in RSA form
-func (r *instanceReconciler) publicKeyRSA() *rsa.PublicKey {
-	// upgrade SSH public key to CryptoPublicKey interface, extract key data, then convert to RSA object
-	sshPubKey := r.signer.PublicKey()
-	cryptoKey := sshPubKey.(ssh.CryptoPublicKey)
-	pubCrypto := cryptoKey.CryptoPublicKey()
-	return pubCrypto.(*rsa.PublicKey)
-}
-
-// ecrypt takes in a string and proceeds to encrypt it using the SSH public key data
-func (r *instanceReconciler) encrypt(plainText string) (string, error) {
-	rsaPubKey := r.publicKeyRSA()
-	encryptedBytes, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, rsaPubKey, []byte(plainText), nil)
-	if err != nil {
-		return "", err
-	}
-
-	return base64.StdEncoding.EncodeToString(encryptedBytes), nil
-}
-
-// privateKeyRSA retrieves the private key used to access Windows instances in RSA form
-func (r *instanceReconciler) privateKeyRSA() (*rsa.PrivateKey, error) {
-	privateKeyBytes, err := secrets.GetPrivateKey(kubeTypes.NamespacedName{Namespace: r.watchNamespace,
-		Name: secrets.PrivateKeySecret}, r.client)
-	if err != nil {
-		return nil, err
-	}
-
-	decodedBlock, _ := pem.Decode(privateKeyBytes)
-	rsaPrivateKey, err := x509.ParsePKCS1PrivateKey(decodedBlock.Bytes)
-	if err != nil {
-		return nil, errors.Wrap(err, "unable to convert SSH private key to RSA standard")
-	}
-
-	return rsaPrivateKey, nil
-}
-
-// decrypt takes in encrypted text and attempts to decrypt the string using the SSH private key data
-func (r *instanceReconciler) decrypt(cipherText string) (string, error) {
-	usernameData, err := base64.StdEncoding.DecodeString(cipherText)
-	if err != nil {
-		return "", err
-	}
-
-	rsaPrivateKey, err := r.privateKeyRSA()
-	decryptedBytes, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, rsaPrivateKey, usernameData, nil)
-	if err != nil {
-		return "", err
-	}
-
-	return string(decryptedBytes), nil
 }
 
 // instanceFromNode returns an instance object for the given node. Requires a username that can be used to SSH into the
