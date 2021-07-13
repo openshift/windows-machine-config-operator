5db0558f95f63d17bd74ed34842f71813e57e8f0
Jose Valdes <jvaldes@redhat.com>
2021-07-13 16:21:48 -0400

    [docs][vsphere_ci] Packer build and scripts updates
    This commit fixes formatting issues and typos, aligns the
    Packer builder script (build.json) with the documentation.
    
    Highlights:
     - build.json file formatted and re-arranged grouping
       relevant keys together
     - `variables` key moved to the top of build.json script
       for easy access during customization
     - `vsphere-network` parameter removed from build.json script
     - `vsphere-folder` parameter renamed to `vm-template-folder`
     - `winadmin-password` parameter renamed to `vm-elevated-password`
     - Increased the provisioner pause to 3 mins
     - Added default values to `os-iso-path` and `vmtools-iso-path`
       parameters to follow the naming convention adopted
       by https://issues.redhat.com/browse/DPP-7909
    
    Updated PowerShell script (install-docker.ps1) to install docker
    and to configure VM Tools (configure-vm-tools.ps1).
    
    Added the NuGet package provider installation command. NuGet must be installed
    first to avoid the manual confirmation prompted while installing the docker
    package.
    See https://docs.microsoft.com/en-us/virtualization/windowscontainers/quick-start/set-up-environment?tabs=Windows-Server#install-docker
    
    To avoid leaving the VM template with an Administrator command prompt at first
    boot, the LogonCount in the autounattend.xml file, was adjusted to value 1.
    See https://docs.microsoft.com/en-us/windows-hardware/customize/desktop/unattend/microsoft-windows-shell-setup-autologon-logoncount#logoncount-known-issue
    
    There is a known issue, where the kubelet is not able to join a node if the
    machine name is invalid, for example contains uppercase letters. To work around
    this, the computer name is fixed to `winhost` in the autounattend.xml file,
    following the Kubernetes' names specification.
    See https://kubernetes.io/docs/concepts/overview/working-with-objects/names

--- docs/vsphere_ci/scripts/install-docker.ps1
+++ docs/vsphere_ci/scripts/install-docker.ps1
@@ -1,3 +1,11 @@
+# Powershell script to install docker
+# https://docs.microsoft.com/en-us/virtualization/windowscontainers/quick-start/set-up-environment?tabs=Windows-Server
+#
+# NuGet provider must be installed first to avoid the manual confirmation prompted while installing the docker package
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+# configure repository policy
 Set-PSRepository PSGallery -InstallationPolicy Trusted
+# install module with provider
 Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
+# install docker package
 Install-Package -Name docker -ProviderName DockerMsftProvider -Force
