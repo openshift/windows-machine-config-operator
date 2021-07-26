bb608be9ce11c2e2324dd83664b80d36b6db7784
Sebastian Soto <ssoto@redhat.com>
2021-07-26 11:31:55 -0400

    Refactor annotation patch creation
    Refactors the patch creation in order to enable better unit testing.
    Specifically, this commit makes it so that metadata.generatePatch
    no longer returns the marshalled patches, but instead returns the slice
    of patches it generated, allowing those to be marshalled at the call
    sites, metadata.GenerateAddPatch() and metadata.GenerateRemovePatch().
    
    This commit is important for two reasons. Firstly, using
    assert.elementsMatch to test that a byte slice is correct does not seem
    like the right thing to do. This seems as if it just tests that the
    chars are all the same, so the patchdata could be made out of order and
    this unit test would not catch that. Secondly this allows for more
    complex testing, which will be required in a future commit where
    generatePatch() handles both annotations and labels.
    
    Additionally, since the tests had to be changed, I changed one case to
    exercise the escaping functionality, something that was being missed by
    the tests.

--- pkg/metadata/metadata_test.go
+++ pkg/metadata/metadata_test.go
@@ -3,91 +3,66 @@ package metadata
 import (
 	"testing"
 
+	"github.com/openshift/windows-machine-config-operator/pkg/patch"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestGenerateAddPatch(t *testing.T) {
+func TestGeneratePatch(t *testing.T) {
 	testCases := []struct {
 		name        string
+		operation   string
 		input       map[string]string
-		expectedOut []byte
+		expectedOut []*patch.JSONPatch
 		expectedErr bool
 	}{
 		{
 			name:        "Annotations nil",
 			input:       nil,
+			operation:   "add",
 			expectedOut: nil,
 			expectedErr: true,
 		},
 		{
 			name:        "Annotations empty",
 			input:       map[string]string{},
+			operation:   "add",
 			expectedOut: nil,
 			expectedErr: true,
 		},
 		{
-			name:        "Single annotation",
-			input:       map[string]string{"annotation-1": "3.0"},
-			expectedOut: []byte("[{\"op\":\"add\",\"path\":\"/metadata/annotations/annotation-1\",\"value\":\"3.0\"}]"),
+			name:      "Single annotation",
+			input:     map[string]string{"annotation-1": "3.0"},
+			operation: "add",
+			expectedOut: []*patch.JSONPatch{{
+				Op:    "add",
+				Path:  "/metadata/annotations/annotation-1",
+				Value: "3.0",
+			}},
 			expectedErr: false,
 		},
 		{
-			name:  "Multiple annotations",
-			input: map[string]string{"annotation-1": "3.0", "annotation-2": "17"},
-			expectedOut: []byte("[{\"op\":\"add\",\"path\":\"/metadata/annotations/annotation-1\",\"value\":\"3.0\"}," +
-				"{\"op\":\"add\",\"path\":\"/metadata/annotations/annotation-2\",\"value\":\"17\"}]"),
+			name:      "Multiple annotations",
+			input:     map[string]string{"annotation-1": "3.0", "escaped/annotation": "17"},
+			operation: "add",
+			expectedOut: []*patch.JSONPatch{
+				{
+					Op:    "add",
+					Path:  "/metadata/annotations/annotation-1",
+					Value: "3.0",
+				},
+				{
+					Op:    "add",
+					Path:  "/metadata/annotations/escaped~1annotation",
+					Value: "17",
+				},
+			},
 			expectedErr: false,
 		},
 	}
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
-			out, err := GenerateAddPatch(test.input)
-			if test.expectedErr {
-				assert.Error(t, err)
-				return
-			}
-			require.NoError(t, err)
-			assert.ElementsMatch(t, test.expectedOut, out)
-		})
-	}
-}
-func TestGenerateRemovePatch(t *testing.T) {
-	testCases := []struct {
-		name        string
-		input       []string
-		expectedOut []byte
-		expectedErr bool
-	}{
-		{
-			name:        "Annotations nil",
-			input:       nil,
-			expectedOut: nil,
-			expectedErr: true,
-		},
-		{
-			name:        "Annotations empty",
-			input:       []string{},
-			expectedOut: nil,
-			expectedErr: true,
-		},
-		{
-			name:        "Single annotation",
-			input:       []string{"annotation-1"},
-			expectedOut: []byte("[{\"op\":\"remove\",\"path\":\"/metadata/annotations/annotation-1\",\"value\":\"\"}]"),
-			expectedErr: false,
-		},
-		{
-			name:  "Multiple annotations",
-			input: []string{"annotation-1", "annotation-2"},
-			expectedOut: []byte("[{\"op\":\"remove\",\"path\":\"/metadata/annotations/annotation-1\",\"value\":\"\"}," +
-				"{\"op\":\"remove\",\"path\":\"/metadata/annotations/annotation-2\",\"value\":\"\"}]"),
-			expectedErr: false,
-		},
-	}
-	for _, test := range testCases {
-		t.Run(test.name, func(t *testing.T) {
-			out, err := GenerateRemovePatch(test.input)
+			out, err := generateAnnotationPatch(test.operation, test.input)
 			if test.expectedErr {
 				assert.Error(t, err)
 				return
