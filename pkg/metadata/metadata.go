16cf810c5ac4d57287d0262545a907713ea25cbc
Sebastian Soto <ssoto@redhat.com>
2021-07-26 11:56:56 -0400

    Generalize annotation patch creation
    Extends the annotation patch creation functions to allow for labels as
    well.

--- pkg/metadata/metadata.go
+++ pkg/metadata/metadata.go
@@ -3,7 +3,7 @@ package metadata
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
+	"path"
 	"strings"
 
 	"github.com/openshift/windows-machine-config-operator/pkg/patch"
@@ -12,45 +12,55 @@ import (
 // VersionAnnotation indicates the version of WMCO that configured the node
 const VersionAnnotation = "windowsmachineconfig.openshift.io/version"
 
-// generateAnnotationPatch creates a patch applying the given operation onto each given annotation key and value
-func generateAnnotationPatch(op string, annotations map[string]string) ([]*patch.JSONPatch, error) {
-	if len(annotations) == 0 {
-		return nil, errors.New("annotations to format cannot be empty or nil")
+// generatePatch creates a patch applying the given operation onto each given annotation key and value
+func generatePatch(op string, labels, annotations map[string]string) ([]*patch.JSONPatch, error) {
+	if len(labels) == 0 && len(annotations) == 0 {
+		return nil, errors.New("labels and annotations empty")
 	}
 	var patches []*patch.JSONPatch
-	for key, value := range annotations {
-		patches = append(patches, patch.NewJSONPatch(op, formatPathValue(key), value))
+	if labels != nil {
+		for key, value := range labels {
+			patches = append(patches, patch.NewJSONPatch(op, path.Join("/metadata/labels/", escape(key)), value))
+		}
+	}
+	if annotations != nil {
+		for key, value := range annotations {
+			patches = append(patches, patch.NewJSONPatch(op, path.Join("/metadata/annotations/", escape(key)), value))
+		}
 	}
 	return patches, nil
 }
 
-// GenerateAddPatch creates a comma-separated list of operations to add all given annotations from an object
-// An "add" patch overwrites existing value if an annotation already exists
-func GenerateAddPatch(annotations map[string]string) ([]byte, error) {
-	patch, err := generateAnnotationPatch("add", annotations)
+// GenerateAddPatch creates a comma-separated list of operations to add all given labels and annotations from an object
+// An "add" patch overwrites existing value if a label or annotation already exists
+func GenerateAddPatch(labels, annotations map[string]string) ([]byte, error) {
+	patch, err := generatePatch("add", labels, annotations)
 	if err != nil {
 		return []byte{}, err
 	}
 	return json.Marshal(patch)
 }
 
-// GenerateRemovePatch creates a comma-separated list of operations to remove all given annotations from an object
-// A "remove" patch fails transactionally if any of the annotations do not exist
-func GenerateRemovePatch(annotations []string) ([]byte, error) {
+// GenerateRemovePatch creates a comma-separated list of operations to remove all given labels and annotations from an
+// object. A "remove" patch fails transactionally if any of the annotations do not exist.
+func GenerateRemovePatch(labels, annotations []string) ([]byte, error) {
+	labelMap := make(map[string]string)
+	for _, label := range labels {
+		labelMap[label] = ""
+	}
 	annotationMap := make(map[string]string)
 	for _, annotation := range annotations {
 		annotationMap[annotation] = ""
 	}
-	patch, err := generateAnnotationPatch("remove", annotationMap)
+	patch, err := generatePatch("remove", labelMap, annotationMap)
 	if err != nil {
 		return []byte{}, err
 	}
 	return json.Marshal(patch)
 }
 
-// formatPathValue formats the path value specifying which attribute a JSON Patch operation should be applied to
-func formatPathValue(key string) string {
-	// The `/` in the annotation key needs to be escaped in order to not be considered a "directory" in the path
-	escapedKey := strings.Replace(key, "/", "~1", -1)
-	return fmt.Sprintf("/metadata/annotations/%s", escapedKey)
+// escape replaces characters which would cause parsing issues with their escaped equivalent
+func escape(key string) string {
+	// The `/` in the metadata key needs to be escaped in order to not be considered a "directory" in the path
+	return strings.Replace(key, "/", "~1", -1)
 }
