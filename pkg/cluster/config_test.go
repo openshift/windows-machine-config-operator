1386959e1f57d9f244ec8b2a911aa786ac89d129
Sebastian Soto <ssoto@redhat.com>
2022-01-12 13:54:47 -0500

    Set v1.23 as the required k8s version
    This commit updates the csv and WMCO code to require kubernetes 1.23 in
    order to be installed and ran.

--- pkg/cluster/config_test.go
+++ pkg/cluster/config_test.go
@@ -125,9 +125,9 @@ func TestIsValidKubernetesVersion(t *testing.T) {
 		error   bool
 	}{
 		{"cluster version lower than supported version ", "v1.17.1", true},
-		{"cluster version equals supported version", "v1.22.0", false},
-		{"cluster version equals supported version", "v1.23.4", false},
-		{"cluster version greater than supported version ", "v1.24.2", true},
+		{"cluster version equals supported version", "v1.23.0", false},
+		{"cluster version equals supported version", "v1.24.4", false},
+		{"cluster version greater than supported version ", "v1.25.2", true},
 	}
 
 	for _, tt := range tests {
