903c08707de55cdc7cf01c7f827982eb63b00bf8
Sebastian Soto <ssoto@redhat.com>
2022-05-04 13:54:17 -0400

    [wicd] Correct issues with winsvc mock interfaces
    Includes correcting the FakeService method declaration by passing by
    reference instead of value and introducing the realMgr type in order to
    use mgr.Mgr through a winsvc.Mgr interface.

--- pkg/daemon/winsvc/winsvc.go
+++ pkg/daemon/winsvc/winsvc.go
@@ -17,6 +17,27 @@ type Mgr interface {
 	OpenService(string) (Service, error)
 }
 
+// realManager is defined as a way for us to redefine the function signatures of mgr.Mgr, so that they can fulfill
+// the Mgr interface. When used directly, functions like mgr.Mgr's CreateService() returns a *mgr.Service type. This
+// causes issues fitting it to the Mgr interface, even though *mgr.Service implements the Service interface. By
+// using the realManager wrapper functions, the underlying mgr.Mgr methods can be called, and then the *mgr.Service
+// return values can be cast to the Service interface.
+type realManager mgr.Mgr
+
+func (m *realManager) CreateService(name, exepath string, config mgr.Config, args ...string) (Service, error) {
+	underlyingMgr := (*mgr.Mgr)(m)
+	service, err := underlyingMgr.CreateService(name, exepath, config, args...)
+	return Service(service), err
+}
+func (m *realManager) ListServices() ([]string, error) {
+	underlyingMgr := (*mgr.Mgr)(m)
+	return underlyingMgr.ListServices()
+}
+func (m *realManager) OpenService(name string) (Service, error) {
+	underlyingMgr := (*mgr.Mgr)(m)
+	return underlyingMgr.OpenService(name)
+}
+
 // fakeServiceList mocks out the state of all services on a Windows instance
 type fakeServiceList struct {
 	m    *sync.Mutex
@@ -79,7 +100,7 @@ type testMgr struct {
 // the service is started; these arguments are distinct from
 // the arguments passed to Service.Start or via the "Start
 // parameters" field in the service's Properties dialog box.
-func (t testMgr) CreateService(name, exepath string, config mgr.Config, args ...string) (Service, error) {
+func (t *testMgr) CreateService(name, exepath string, config mgr.Config, args ...string) (Service, error) {
 	// Throw an error if the service already exists
 	if _, ok := t.svcList.read(name); ok {
 		return nil, errors.New("service already exists")
@@ -97,11 +118,11 @@ func (t testMgr) CreateService(name, exepath string, config mgr.Config, args ...
 	return &service, nil
 }
 
-func (t testMgr) ListServices() ([]string, error) {
+func (t *testMgr) ListServices() ([]string, error) {
 	return t.svcList.listServiceNames(), nil
 }
 
-func (t testMgr) OpenService(name string) (Service, error) {
+func (t *testMgr) OpenService(name string) (Service, error) {
 	service, exists := t.svcList.read(name)
 	if !exists {
 		return nil, fmt.Errorf("service does not exist")
@@ -126,24 +147,24 @@ type FakeService struct {
 	serviceList *fakeServiceList
 }
 
-func (f FakeService) Delete() error {
+func (f *FakeService) Delete() error {
 	return f.serviceList.remove(f.name)
 }
 
-func (f FakeService) Close() error {
+func (f *FakeService) Close() error {
 	return nil
 }
 
-func (f FakeService) Start(_ ...string) error {
+func (f *FakeService) Start(_ ...string) error {
 	f.status.State = svc.Running
 	return nil
 }
 
-func (f FakeService) Config() (mgr.Config, error) {
+func (f *FakeService) Config() (mgr.Config, error) {
 	return f.config, nil
 }
 
-func (f FakeService) Control(cmd svc.Cmd) (svc.Status, error) {
+func (f *FakeService) Control(cmd svc.Cmd) (svc.Status, error) {
 	switch cmd {
 	case svc.Stop:
 		f.status.State = svc.Stopped
@@ -151,17 +172,30 @@ func (f FakeService) Control(cmd svc.Cmd) (svc.Status, error) {
 	return f.status, nil
 }
 
-func (f FakeService) Query() (svc.Status, error) {
+func (f *FakeService) Query() (svc.Status, error) {
 	return f.status, nil
 }
 
-func (f FakeService) UpdateConfig(config mgr.Config) error {
+func (f *FakeService) UpdateConfig(config mgr.Config) error {
 	f.config = config
 	return nil
 }
 
-func NewMgr() (*mgr.Mgr, error) {
-	return mgr.Connect()
+func NewFakeService(name string, config mgr.Config, status svc.Status) *FakeService {
+	return &FakeService{
+		name:   name,
+		config: config,
+		status: status,
+	}
+}
+
+func NewMgr() (Mgr, error) {
+	manager, err := mgr.Connect()
+	if err != nil {
+		return nil, err
+	}
+
+	return (*realManager)(manager), nil
 }
 
 func NewTestMgr(existingServices map[string]*FakeService) *testMgr {
