82eb0ada4130c9ea22b4a35760846f67c31ae845
Sebastian Soto <ssoto@redhat.com>
2022-05-16 17:41:05 -0400

    [wicd] Properly model Service dependencies
    This commit includes various changes with how the WICD test code
    handles service dependencies. Starting or stopping a fake service
    will now properly model the Windows service manager behavior.
    Starting a service with dependencies will start the dependencies as
    well. Stopping a service which other services are dependent on will
    cause the stop command to fail.

--- pkg/daemon/winsvc/winsvc.go
+++ pkg/daemon/winsvc/winsvc.go
@@ -159,6 +159,17 @@ func (f *FakeService) Start(_ ...string) error {
 	if f.status.State == svc.Running {
 		return errors.New("service already running")
 	}
+	// each of the service's dependencies must be started before the service is started
+	for _, dependency := range f.config.Dependencies {
+		dependencyService, present := f.serviceList.read(dependency)
+		if !present {
+			return errors.New("dependent service doesnt exist")
+		}
+		// Windows will attempt to start the service only if it is not already running
+		if err := EnsureServiceState(dependencyService, svc.Running); err != nil {
+			return err
+		}
+	}
 	f.status.State = svc.Running
 	return nil
 }
@@ -172,6 +183,33 @@ func (f *FakeService) Control(cmd svc.Cmd) (svc.Status, error) {
 	case svc.Stop:
 		if f.status.State == svc.Stopped {
 			return svc.Status{}, errors.New("service already stopped")
+		}
+		// Windows has a hard time stopping services that other services are dependent on. To most safely model this
+		// functionality it is better to make it so that our mock manager is completely unable to stop services in that
+		// scenario.
+		existingServices := f.serviceList.listServiceNames()
+		for _, serviceName := range existingServices {
+			service, present := f.serviceList.read(serviceName)
+			if !present {
+				return svc.Status{}, errors.New("unable to open service " + serviceName)
+			}
+			config, err := service.Config()
+			if err != nil {
+				return svc.Status{}, errors.Wrapf(err, "error getting %s service config", serviceName)
+			}
+			for _, dependency := range config.Dependencies {
+				// Found a service that has this one as a dependency, ensure it is not running
+				if dependency == f.name {
+					status, err := service.Query()
+					if err != nil {
+						return svc.Status{}, errors.Wrapf(err, "error querying %s service status", serviceName)
+					}
+					if status.State != svc.Stopped {
+						return svc.Status{}, errors.New("cannot stop service as other service " + serviceName +
+							" is dependent on it")
+					}
+				}
+			}
 		}
 		f.status.State = svc.Stopped
 	}
--- pkg/daemon/winsvc/winsvc_test.go
+++ pkg/daemon/winsvc/winsvc_test.go
@@ -3,6 +3,8 @@
 package winsvc
 
 import (
+	"errors"
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -175,82 +177,232 @@ func TestDeleteService(t *testing.T) {
 }
 
 func TestStartService(t *testing.T) {
+	svcName := "testsvc"
 	testIO := []struct {
 		name          string
-		startingState svc.State
-		expectedState svc.State
+		startingState map[string]*FakeService
+		expectedState map[string]Service
 		expectErr     bool
 	}{
 		{
 			name:          "service not running",
-			startingState: svc.Stopped,
-			expectedState: svc.Running,
+			startingState: map[string]*FakeService{svcName: {name: svcName, status: svc.Status{State: svc.Stopped}}},
+			expectedState: map[string]Service{svcName: &FakeService{name: svcName, status: svc.Status{State: svc.Running}}},
 			expectErr:     false,
 		},
 		{
+			name: "service not running and dependency running",
+			startingState: map[string]*FakeService{
+				svcName: {
+					name: svcName, status: svc.Status{State: svc.Stopped}, config: mgr.Config{Dependencies: []string{"service2"}},
+				},
+				"service2": {
+					name: svcName, status: svc.Status{State: svc.Running},
+				},
+			},
+			expectedState: map[string]Service{
+				svcName: &FakeService{
+					name: svcName, status: svc.Status{State: svc.Running}, config: mgr.Config{Dependencies: []string{"service2"}},
+				},
+				"service2": &FakeService{
+					name: svcName, status: svc.Status{State: svc.Running},
+				},
+			},
+			expectErr: false,
+		},
+		{
+			name: "service and dependency not running",
+			startingState: map[string]*FakeService{
+				svcName: {
+					name: svcName, status: svc.Status{State: svc.Stopped}, config: mgr.Config{Dependencies: []string{"service2"}},
+				},
+				"service2": {
+					name: svcName, status: svc.Status{State: svc.Stopped},
+				},
+			},
+			expectedState: map[string]Service{
+				svcName: &FakeService{
+					name: svcName, status: svc.Status{State: svc.Running}, config: mgr.Config{Dependencies: []string{"service2"}},
+				},
+				"service2": &FakeService{
+					name: svcName, status: svc.Status{State: svc.Running},
+				},
+			},
+			expectErr: false,
+		},
+		{
+			name: "service and dependency not running, dependency not startable",
+			startingState: map[string]*FakeService{
+				svcName: {
+					name: svcName, status: svc.Status{State: svc.Stopped}, config: mgr.Config{Dependencies: []string{"service2"}},
+				},
+				"service2": {
+					name: svcName, status: svc.Status{State: svc.Stopped}, config: mgr.Config{Dependencies: []string{"doesnt-exist"}},
+				},
+			},
+			expectedState: map[string]Service{
+				svcName: &FakeService{
+					name: svcName, status: svc.Status{State: svc.Stopped}, config: mgr.Config{Dependencies: []string{"service2"}},
+				},
+				"service2": &FakeService{
+					name: svcName, status: svc.Status{State: svc.Stopped}, config: mgr.Config{Dependencies: []string{"doesnt-exist"}},
+				},
+			},
+			expectErr: true,
+		},
+		{
+			name: "service and dependency not running, service not startable",
+			startingState: map[string]*FakeService{
+				svcName: {
+					name: svcName, status: svc.Status{State: svc.Stopped}, config: mgr.Config{Dependencies: []string{"service2", "doesnt-exist"}},
+				},
+				"service2": {
+					name: svcName, status: svc.Status{State: svc.Stopped},
+				},
+			},
+			expectedState: map[string]Service{
+				svcName: &FakeService{
+					name: svcName, status: svc.Status{State: svc.Stopped}, config: mgr.Config{Dependencies: []string{"service2", "doesnt-exist"}},
+				},
+				"service2": &FakeService{
+					name: svcName, status: svc.Status{State: svc.Running},
+				},
+			},
+			expectErr: true,
+		},
+		{
 			name:          "service running",
-			startingState: svc.Running,
-			expectedState: svc.Running,
+			startingState: map[string]*FakeService{svcName: {name: svcName, status: svc.Status{State: svc.Running}}},
+			expectedState: map[string]Service{svcName: &FakeService{name: svcName, status: svc.Status{State: svc.Running}}},
 			expectErr:     true,
 		},
 	}
 	for _, test := range testIO {
 		t.Run(test.name, func(t *testing.T) {
-			svcName := "testsvc"
-			svcs := map[string]*FakeService{svcName: {name: svcName, status: svc.Status{State: test.startingState}}}
-			manager := NewTestMgr(svcs)
+			manager := NewTestMgr(test.startingState)
 			service, err := manager.OpenService(svcName)
 			require.NoError(t, err)
 			err = service.Start()
 			if test.expectErr {
 				assert.Error(t, err)
-				return
+			} else {
+				require.NoError(t, err)
 			}
+			listsEqual, err := serviceListsEqual(test.expectedState, manager.svcList.svcs)
 			require.NoError(t, err)
-			newStatus, err := service.Query()
-			require.NoError(t, err)
-			assert.Equal(t, test.expectedState, newStatus.State)
+			assert.True(t, listsEqual)
 		})
 	}
 }
+
 func TestStopService(t *testing.T) {
+	svcName := "testsvc"
 	testIO := []struct {
 		name          string
-		startingState svc.State
-		expectedState svc.State
+		startingState map[string]*FakeService
+		expectedState map[string]Service
 		expectErr     bool
 	}{
 		{
 			name:          "service not running",
-			startingState: svc.Stopped,
-			expectedState: svc.Stopped,
+			startingState: map[string]*FakeService{svcName: {name: svcName, status: svc.Status{State: svc.Stopped}}},
+			expectedState: map[string]Service{svcName: &FakeService{name: svcName, status: svc.Status{State: svc.Stopped}}},
 			expectErr:     true,
 		},
 		{
+			name: "service running and dependent service not running",
+			startingState: map[string]*FakeService{
+				svcName: {
+					name: svcName, status: svc.Status{State: svc.Running},
+				},
+				"service2": {
+					name: svcName, status: svc.Status{State: svc.Stopped}, config: mgr.Config{Dependencies: []string{svcName}},
+				},
+			},
+			expectedState: map[string]Service{
+				svcName: &FakeService{
+					name: svcName, status: svc.Status{State: svc.Stopped},
+				},
+				"service2": &FakeService{
+					name: svcName, status: svc.Status{State: svc.Stopped}, config: mgr.Config{Dependencies: []string{svcName}},
+				},
+			},
+			expectErr: false,
+		},
+		{
+			name: "service and dependent service running",
+			startingState: map[string]*FakeService{
+				svcName: {
+					name: svcName, status: svc.Status{State: svc.Running},
+				},
+				"service2": {
+					name: svcName, status: svc.Status{State: svc.Running}, config: mgr.Config{Dependencies: []string{svcName}},
+				},
+			},
+			expectedState: map[string]Service{
+				svcName: &FakeService{
+					name: svcName, status: svc.Status{State: svc.Running},
+				},
+				"service2": &FakeService{
+					name: svcName, status: svc.Status{State: svc.Running}, config: mgr.Config{Dependencies: []string{svcName}},
+				},
+			},
+			expectErr: true,
+		},
+		{
 			name:          "service running",
-			startingState: svc.Running,
-			expectedState: svc.Stopped,
+			startingState: map[string]*FakeService{svcName: {name: svcName, status: svc.Status{State: svc.Running}}},
+			expectedState: map[string]Service{svcName: &FakeService{name: svcName, status: svc.Status{State: svc.Stopped}}},
 			expectErr:     false,
 		},
 	}
 	for _, test := range testIO {
 		t.Run(test.name, func(t *testing.T) {
-			svcName := "testsvc"
-			svcs := map[string]*FakeService{svcName: {name: svcName, status: svc.Status{State: test.startingState}}}
-			manager := NewTestMgr(svcs)
+			manager := NewTestMgr(test.startingState)
 			service, err := manager.OpenService(svcName)
 			require.NoError(t, err)
 			_, err = service.Control(svc.Stop)
 			if test.expectErr {
 				assert.Error(t, err)
-				return
+			} else {
+				require.NoError(t, err)
 			}
+			listsEqual, err := serviceListsEqual(test.expectedState, manager.svcList.svcs)
 			require.NoError(t, err)
-			newStatus, err := service.Query()
-			require.NoError(t, err)
-			assert.Equal(t, test.expectedState, newStatus.State)
+			assert.True(t, listsEqual)
 		})
 	}
+}
+
+// serviceListsEqual returns true if the two maps are equivalent
+func serviceListsEqual(m1, m2 map[string]Service) (bool, error) {
+	if len(m1) != len(m2) {
+		return false, nil
+	}
+	for m1ServiceName, m1Service := range m1 {
+		m1FakeService, ok := m1Service.(*FakeService)
+		if !ok {
+			return false, errors.New("service not castable to *FakeService")
+		}
+		m2Service, present := m2[m1ServiceName]
+		if !present {
+			return false, nil
+		}
+		m2FakeService, ok := m2Service.(*FakeService)
+		if !ok {
+			return false, errors.New("service not castable to *FakeService")
+		}
+		// Differences in the serviceList pointers will cause DeepEqual() to return false, so make a copy of each
+		// service and set the serviceList pointer to nil
+		m1FakeServiceCopy := *m1FakeService
+		m2FakeServiceCopy := *m2FakeService
+		m1FakeServiceCopy.serviceList = nil
+		m2FakeServiceCopy.serviceList = nil
+		if !reflect.DeepEqual(m1FakeServiceCopy, m2FakeServiceCopy) {
+			return false, nil
+		}
+	}
+	return true, nil
 }
 
 func TestEnsureService(t *testing.T) {
