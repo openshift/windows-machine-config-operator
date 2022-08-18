014972fd12cfee74a80c5a5235d81c643fffb166
Mohammad Saif Shaikh <mohashai@redhat.com>
2022-08-18 16:13:23 -0400

    [winsvc] Wait for service state changes
    This commit adds a wait to EnsureServiceState, ensuring that the Windows
    service actually has the desired state when the function returns. This makes
    any operations that occur after calling this function safe (for example,
    deleting after changing state to stopped).
    
    A new constant is added to specify the wait interval between Windows API calls.

--- pkg/daemon/winsvc/winsvc.go
+++ pkg/daemon/winsvc/winsvc.go
@@ -6,6 +6,9 @@ import (
 	"github.com/pkg/errors"
 	"golang.org/x/sys/windows/svc"
 	"golang.org/x/sys/windows/svc/mgr"
+	"k8s.io/apimachinery/pkg/util/wait"
+
+	"github.com/openshift/windows-machine-config-operator/pkg/retry"
 )
 
 type Service interface {
@@ -28,11 +31,27 @@ func EnsureServiceState(service Service, state svc.State) error {
 	}
 	switch state {
 	case svc.Running:
-		return service.Start()
+		err = service.Start()
+		if err != nil {
+			return err
+		}
 	case svc.Stopped:
 		_, err = service.Control(svc.Stop)
-		return err
+		if err != nil {
+			return err
+		}
 	default:
 		return errors.New("unexpected state request")
 	}
+	// Wait for the state change to actually take place
+	return wait.PollImmediate(retry.WindowsAPIInterval, retry.ResourceChangeTimeout, func() (bool, error) {
+		status, err := service.Query()
+		if err != nil {
+			return false, errors.Wrap(err, "error querying service state")
+		}
+		if status.State == state {
+			return true, nil
+		}
+		return false, nil
+	})
 }
