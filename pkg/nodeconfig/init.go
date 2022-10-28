a57bb388ddd83c510bdd8ed48f90ba84263be95e
Mohammad Saif Shaikh <mohashai@redhat.com>
2022-10-28 16:26:53 -0400

    Bootstrap nodes using WICD
    With this commit, WMCB is no longer invoked to start kubelet and create node
    objects. Instead, WMCO invokes WICD's `bootstrap` command.
    
    The WICD service account credentials are now read from the watch namespace
    rather than hardcoded. The value is cached for repeated use each reconcilition
    as it cannot change during the lifetime of the operator pod.

--- pkg/nodeconfig/init.go
+++ pkg/nodeconfig/init.go
@@ -1,7 +1,19 @@
 package nodeconfig
 
 import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/pkg/errors"
+	core "k8s.io/api/core/v1"
+	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 	ctrl "sigs.k8s.io/controller-runtime"
+	crclientcfg "sigs.k8s.io/controller-runtime/pkg/client/config"
+
+	"github.com/openshift/windows-machine-config-operator/pkg/windows"
 )
 
 // cache holds the information of the nodeConfig that is invariant for multiple reconciliation cycles. We'll use this
@@ -17,6 +29,8 @@ type cache struct {
 	workerIgnitionEndPoint string
 	// apiServerEndpoint is the address which clients can interact with the API server through
 	apiServerEndpoint string
+	// credentials holds a certificate and token needed to interact with the API server
+	credentials *windows.Authentication
 }
 
 // cache has the information related to nodeConfig that should not be changed.
@@ -40,4 +54,50 @@ func init() {
 	// populate the cache
 	nodeConfigCache.apiServerEndpoint = kubeAPIServerEndpoint
 	nodeConfigCache.workerIgnitionEndPoint = "https://" + clusterAddress + ":22623/config/worker"
+	nodeConfigCache.credentials, err = getWICDCredentials()
+	if err != nil {
+		log.Error(err, "unable to get WICD service account credentials")
+	}
+}
+
+// getWICDCredentials returns the CA cert and access token associated with the WICD service account
+func getWICDCredentials() (*windows.Authentication, error) {
+	cfg, err := crclientcfg.GetConfig()
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to get config to talk to kubernetes api server")
+	}
+	client, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to get client from the given config")
+	}
+
+	operatorNamespaceVar := "WATCH_NAMESPACE"
+	wmcoNamespace, found := os.LookupEnv(operatorNamespaceVar)
+	if !found {
+		return nil, errors.Errorf("operator namespace must be set in %s", operatorNamespaceVar)
+	}
+
+	secrets, err := client.CoreV1().Secrets(wmcoNamespace).List(context.TODO(), meta.ListOptions{})
+	if err != nil {
+		return nil, errors.Wrapf(err, "error listing secrets in namespace %s", wmcoNamespace)
+	}
+	tokenSecretPrefix := "windows-instance-config-daemon-token-"
+	var filteredSecrets []core.Secret
+	for _, secret := range secrets.Items {
+		if strings.HasPrefix(secret.Name, tokenSecretPrefix) {
+			filteredSecrets = append(filteredSecrets, secret)
+		}
+	}
+	if len(filteredSecrets) != 1 {
+		return nil, fmt.Errorf("expected 1 secret with '%s' prefix, found %d", tokenSecretPrefix, len(filteredSecrets))
+	}
+	caCert := filteredSecrets[0].Data[core.ServiceAccountRootCAKey]
+	if len(caCert) == 0 {
+		return nil, errors.Errorf("WICD ServiceAccount %s data not found", core.ServiceAccountRootCAKey)
+	}
+	token := filteredSecrets[0].Data[core.ServiceAccountTokenKey]
+	if len(token) == 0 {
+		return nil, errors.Errorf("WICD ServiceAccount %s data not found", core.ServiceAccountTokenKey)
+	}
+	return &windows.Authentication{CaCert: caCert, Token: token}, nil
 }
