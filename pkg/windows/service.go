e97fc0056f04909ed56e7da83f9da198163d222b
Sebastian Soto <ssoto@redhat.com>
2023-03-01 10:09:43 -0500

    WICD restarts after program crash
    Sets the recovery actions for WICD using 'sc.exe failure'. This
    instructs the Windows service manager to restart WICD in case the
    program crashes.

--- pkg/windows/service.go
+++ pkg/windows/service.go
@@ -2,6 +2,17 @@ package windows
 
 import "github.com/pkg/errors"
 
+type recoveryActionType string
+
+const serviceRestart = recoveryActionType("restart")
+
+type recoveryAction struct {
+	// actionType is the action that will be performed by the Windows service manager after a program crash
+	actionType recoveryActionType
+	// delay is the amount of time in seconds to wait before performing the specified action
+	delay int
+}
+
 // service struct contains the service information
 type service struct {
 	// binaryPath is the path to the binary to be ran as a service
@@ -12,17 +23,27 @@ type service struct {
 	args string
 	// dependencies is a list of the names of the services that this service is dependent on
 	dependencies []string
+	// recoveryActions is a list of recovery actions that the service manager will apply in case of program crash
+	// these actions will be run in order, until the crash counter is reset
+	recoveryActions []recoveryAction
+	// recoveryPeriod is the amount of time in seconds with no failures after which the recoveryAction crash counter
+	// resets
+	recoveryPeriod int
 }
 
-// newService initializes and returns a pointer to the service struct
-func newService(binaryPath, name, args string, dependencies []string) (*service, error) {
+// newService initializes and returns a pointer to the service struct. The dependencies, recoveryActions, and
+// recoveryPeriod arguments are optional
+func newService(binaryPath, name, args string, dependencies []string, recoveryActions []recoveryAction,
+	recoveryPeriod int) (*service, error) {
 	if binaryPath == "" || name == "" {
 		return nil, errors.Errorf("can't instantiate a service with incomplete service parameters")
 	}
 	return &service{
-		binaryPath:   binaryPath,
-		name:         name,
-		args:         args,
-		dependencies: dependencies,
+		binaryPath:      binaryPath,
+		name:            name,
+		args:            args,
+		dependencies:    dependencies,
+		recoveryActions: recoveryActions,
+		recoveryPeriod:  recoveryPeriod,
 	}, nil
 }
