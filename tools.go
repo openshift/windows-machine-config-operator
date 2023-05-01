374c6d08b5a150aaea747befc65a27275dc90ca5
Aravindh Puthiyaparambil <aravindh@redhat.com>
2023-05-01 12:40:23 -0700

    Add goio
    - Ran "go mod tidy && go mod vendor && go mod verify" after adding goio
      to tools.go
    - Added a goio config file

--- tools.go
+++ tools.go
@@ -3,3 +3,7 @@
 // Place any runtime dependencies as imports in this file.
 // Go modules will be forced to download and install them.
 package tools
+
+import (
+	_ "github.com/go-imports-organizer/goio"
+)
