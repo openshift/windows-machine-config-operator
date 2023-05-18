056eeb945f57146c7e23478ca53b9c06e6a46fb7
Sebastian Soto <ssoto@redhat.com>
2023-05-18 09:53:04 -0400

    Reuse testContext within each test suite
    Previously many subtests were creating their own testContext. Everytime
    a testContext is created there is a cost of initializing multiple k8s
    API clients, as well as the Provider struct used by the testContext.
    But more importantly doing this is causing a lot of boilerplate code at
    the start of each subtest, to initialize the testContext and check for
    an error.
    
    This commit addresses that by creating a testContext at each top level
    test for each test suite. The subtests have been made methods of
    testContext, a pattern that was being used by some of the subtests
    already.
    
    This also makes the reciever of testContext consistent across the tests,
    using `tc` instead of the mix of `tc` and `testCtx` that was present
    before.
    
    (cherry picked from commit 3fc037b6cf8378b94a2e1d1941420e82d84fc13a)

--- test/e2e/reconfigure_test.go
+++ test/e2e/reconfigure_test.go
@@ -17,11 +17,11 @@ import (
 )
 
 func reconfigurationTestSuite(t *testing.T) {
-	testCtx, err := NewTestContext()
+	tc, err := NewTestContext()
 	require.NoError(t, err)
-	t.Run("Reconfigure instance", reconfigurationTest)
-	if testCtx.CloudProvider.GetType() == config.VSpherePlatformType {
-		t.Run("Re-add removed instance", testReAddInstance)
+	t.Run("Reconfigure instance", tc.reconfigurationTest)
+	if tc.CloudProvider.GetType() == config.VSpherePlatformType {
+		t.Run("Re-add removed instance", tc.testReAddInstance)
 	}
 	t.Run("Change private key", testPrivateKeyChange)
 }
@@ -30,48 +30,42 @@ func reconfigurationTestSuite(t *testing.T) {
 // again. In practice, this exact scenario should not happen, however it simulates a similar scenario where an instance
 // was almost completely configured, an error occurred, and the instance is requeued. This is a scenario that should be
 // expected to be ran into often enough, for reasons such as network instability. For that reason this test is warranted.
-func reconfigurationTest(t *testing.T) {
-	testCtx, err := NewTestContext()
+func (tc *testContext) reconfigurationTest(t *testing.T) {
+	machineNodes, err := tc.listFullyConfiguredWindowsNodes(false)
 	require.NoError(t, err)
-
-	machineNodes, err := testCtx.listFullyConfiguredWindowsNodes(false)
-	require.NoError(t, err)
-	byohNodes, err := testCtx.listFullyConfiguredWindowsNodes(true)
+	byohNodes, err := tc.listFullyConfiguredWindowsNodes(true)
 	require.NoError(t, err)
 
 	// Remove the version annotation of one of each type of node
 	patchData, err := metadata.GenerateRemovePatch([]string{}, []string{metadata.VersionAnnotation})
 	require.NoError(t, err)
-	_, err = testCtx.client.K8s.CoreV1().Nodes().Patch(context.TODO(), machineNodes[0].Name, types.JSONPatchType,
+	_, err = tc.client.K8s.CoreV1().Nodes().Patch(context.TODO(), machineNodes[0].Name, types.JSONPatchType,
 		patchData, metav1.PatchOptions{})
 	require.NoError(t, err)
 	// Ensure operator communicates to OLM that upgrade is not safe when processing Machine nodes
-	err = testCtx.validateUpgradeableCondition(metav1.ConditionFalse)
+	err = tc.validateUpgradeableCondition(metav1.ConditionFalse)
 	require.NoError(t, err, "operator Upgradeable condition not in proper state")
 
-	_, err = testCtx.client.K8s.CoreV1().Nodes().Patch(context.TODO(), byohNodes[0].Name, types.JSONPatchType,
+	_, err = tc.client.K8s.CoreV1().Nodes().Patch(context.TODO(), byohNodes[0].Name, types.JSONPatchType,
 		patchData, metav1.PatchOptions{})
 	require.NoError(t, err)
 
 	// The Windows nodes should eventually be returned to the state we expect them to be in
-	err = testCtx.waitForWindowsNodes(gc.numberOfMachineNodes, false, true, false)
+	err = tc.waitForWindowsNodes(gc.numberOfMachineNodes, false, true, false)
 	assert.NoError(t, err, "error waiting for Windows Machine nodes to be reconfigured")
 
-	err = testCtx.waitForWindowsNodes(gc.numberOfBYOHNodes, false, true, true)
+	err = tc.waitForWindowsNodes(gc.numberOfBYOHNodes, false, true, true)
 	assert.NoError(t, err, "error waiting for Windows BYOH nodes to be reconfigured")
 
-	err = testCtx.validateUpgradeableCondition(metav1.ConditionTrue)
+	err = tc.validateUpgradeableCondition(metav1.ConditionTrue)
 	require.NoError(t, err, "operator Upgradeable condition not in proper state")
 }
 
 // testReAddInstance tests the case where a Windows BYOH instance was removed from the cluster, and then re-added.
-func testReAddInstance(t *testing.T) {
+func (tc *testContext) testReAddInstance(t *testing.T) {
 	if gc.numberOfBYOHNodes == 0 {
 		t.Skip("BYOH testing disabled")
 	}
-
-	tc, err := NewTestContext()
-	require.NoError(t, err)
 
 	windowsInstances, err := tc.client.K8s.CoreV1().ConfigMaps(wmcoNamespace).Get(context.TODO(), wiparser.InstanceConfigMap,
 		metav1.GetOptions{})
