7ebecbaf4e65e4e3cf3f644db2965d5b644332d3
Mohammad Saif Shaikh <mohashai@redhat.com>
2024-01-17 14:02:37 -0500

    [scripts] Add metrics port rule to packer script
    Update the firewall rules packer script to explicity allow incoming traffic
    for the Windows Exporter metrics server running on Windows nodes.

--- docs/vsphere_ci/scripts/install-firewall-rules.ps1
+++ docs/vsphere_ci/scripts/install-firewall-rules.ps1
@@ -5,6 +5,8 @@
 
 # create firewall rule to allow Container Logs on port 10250
 New-NetFirewallRule -DisplayName "ContainerLogsPort" -LocalPort 10250 -Enabled True -Direction Inbound -Protocol TCP -Action Allow -EdgeTraversalPolicy Allow
+# create new firewall rule to allow incoming connections on port 9182 for node and pod metrics collection to function
+New-NetFirewallRule -DisplayName "WindowsExporter" -LocalPort 9182 -Enabled True -Direction Inbound -Protocol TCP -Action Allow -EdgeTraversalPolicy Allow
 
 # success
 exit 0
