5b55620fd7001cb81273080db9e1ec8abaa55661
Jose Valdes <jvaldes@redhat.com>
2026-07-29 12:22:23 -0400

    [bundle] Adjust k8s version to 1.36

--- pkg/cluster/config_test.go
+++ pkg/cluster/config_test.go
@@ -125,9 +125,9 @@ func TestIsValidKubernetesVersion(t *testing.T) {
 		error   bool
 	}{
 		{"cluster version lower than supported version ", "v1.17.1", true},
-		{"cluster version equals supported version", "v1.35.0", false},
-		{"cluster version equals supported version", "v1.35.4", false},
-		{"cluster version greater than supported version ", "v1.37.2", true},
+		{"cluster version equals supported version", "v1.36.0", false},
+		{"cluster version equals supported version", "v1.36.4", false},
+		{"cluster version greater than supported version ", "v1.38.2", true},
 	}
 
 	for _, tt := range tests {
