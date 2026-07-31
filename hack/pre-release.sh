20045bc91c581bfa863fad69fee2ee93d8a1479d
Jose Valdes <jvaldes@redhat.com>
2026-07-31 11:55:43 -0400

    [hack] restores createdAt placeholder
    This commit adds a sed command that restores createdAt: REPLACE_DATE after
    make bundle replaces it with an actual timestamp. This sits alongside the
    two existing artifact cleanup commands that already handle REPLACE_IMAGE
    and the operator-sdk version.

--- hack/pre-release.sh
+++ hack/pre-release.sh
@@ -230,6 +230,7 @@ github_update() {
   # remove make bundle artifacts
   sed -i 's/REPLACE_IMAGE:latest/REPLACE_IMAGE/' bundle/manifests/windows-machine-config-operator.clusterserviceversion.yaml
   sed -i "s/operator-sdk-v1.14.0+git/operator-sdk-$OPERATOR_SDK_VERSION+git/" bundle/manifests/windows-machine-config-operator.clusterserviceversion.yaml
+  sed -i 's/createdAt: ".*"/createdAt: REPLACE_DATE/' bundle/manifests/windows-machine-config-operator.clusterserviceversion.yaml
 
   commit_message="[$base_branch] Update version to $updated_version
 
